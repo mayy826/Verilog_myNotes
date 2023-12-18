f02ea7e2c6fca3371638aedab7816210fb64fbeb
mayy826 <52116916+mayy826@users.noreply.github.com>
2023-12-18 12:19:12 +0800

    Update synch_fifo.v

--- fifo/synch_fifo.v
+++ fifo/synch_fifo.v
@@ -1,199 +1,127 @@
 `timescale 1ns / 1ps
 
-module synch_fifo
-#(
-    //----------------------------------
-    // Parameter Declarations
-    //----------------------------------
-    parameter FIFO_PTR              = 3             , //需要幾個bit去指向這8個register,2^3=8,3個bits
-    parameter FIFO_WIDTH            = 16            , //這資料幾bits
-    parameter FIFO_DEPTH            = 8               //有多少個register
-)
-(
-    //----------------------------------
-    // IO Declarations
-    //----------------------------------
-    input                           clk             ,
-    input                           rst_n           ,
-    input                           write_en        ,
-    input [FIFO_WIDTH-1:0]          write_data      ,
-    input                           read_en         ,
-    output [FIFO_WIDTH-1:0]         read_data       ,
-    output reg                      full            ,
-    output reg                      empty           ,
-    output reg [FIFO_PTR:0]         room_avail      ,
-    output [FIFO_PTR:0]             data_avail  
-);
+module synch_fifo( clk, reset, rd_en, wr_en, write_data, read_data, full, empty, full_nxt, empty_nxt, room_avail, data_avail,memory_wire);
 
-    //----------------------------------
-    // Local Parameter Declarations
-    //----------------------------------
-    localparam FIFO_DEPTH_MINUS1    = FIFO_DEPTH-1  ;
+	parameter FIFO_width = 16,
+		  FIFO_depth = 8 ,
+		  FIFO_ptr   = 3 ;
+	
+	input		   				clk, reset         ;
+	input	 	   				rd_en, wr_en	   ;
+	input	[FIFO_width-1:0] 	write_data 			   ; 
+	output	[FIFO_width-1:0] 	read_data 			   ;
+	output		   				full, empty 	   ; 
+	output		   				full_nxt, empty_nxt;
+	output	[FIFO_ptr:0]  		room_avail, data_avail 	 	   ;
+	output  [FIFO_width-1:0] 	memory_wire			   ;
 
-    //----------------------------------
-    // Variable Declarations
-    //----------------------------------
-    reg [FIFO_PTR-1:0]              wr_ptr          ; //宣告要寫入哪個address的register
-    reg [FIFO_PTR-1:0]              wr_ptr_nxt      ;
+			
+	
+	
+	//write-pointer control logic	依序寫register
+	reg	[FIFO_ptr-1:0]wr_reg_ptr;
+	reg 	[FIFO_ptr-1:0]wr_reg_ptr_nxt;
+		
+	always@(*)
+	begin	
+		wr_reg_ptr_nxt = wr_reg_ptr ;
+		
+		if(wr_en == 1'b1)
+			if(wr_reg_ptr == FIFO_depth-1)
+				wr_reg_ptr_nxt <= 'd0;
+			else
+				wr_reg_ptr_nxt <= wr_reg_ptr + 1'b1;		
+	end
+	
+	
+	//read-pointer control logic 依序讀register	
+	reg	[FIFO_ptr-1:0]rd_reg_ptr;
+	reg 	[FIFO_ptr-1:0]rd_reg_ptr_nxt;
+		
+	always@(*)
+	begin		
+		rd_reg_ptr_nxt = rd_reg_ptr ;//把上一個clk前的ptr存進nxt
+		if(rd_en == 1'b1)
+		   if (rd_reg_ptr == FIFO_depth-1)
+			rd_reg_ptr_nxt = 'd0;
+		   else
+			rd_reg_ptr_nxt = rd_reg_ptr + 1'b1;		
+	end
+	
+	// calculate number of  entries in the FIFO
+	reg		[FIFO_ptr:0] num_entries;
+	reg		[FIFO_ptr:0] num_entries_nxt;
+		
+	always@(*)
+	begin
+		num_entries_nxt = num_entries;
+		
+		if( wr_en == 1'b1 && rd_en==1'b1 )
+			num_entries_nxt = num_entries;
+		else if (wr_en == 1'b1)
+			num_entries_nxt = num_entries + 1'b1;
+		else if (rd_en == 1'b1)
+			num_entries_nxt = num_entries - 1'b1;	
+				
+	end
 
-    reg [FIFO_PTR-1:0]              rd_ptr          ;//宣告讀哪個address的register
-    reg [FIFO_PTR-1:0]              rd_ptr_nxt      ;
+	
+	reg 	full, empty;
+	reg		[FIFO_ptr:0] room_avail;
+	wire	full_nxt, empty_nxt;
+	wire	[FIFO_ptr:0] data_avail,room_avail_nxt;
+	
+	assign  full_nxt  		= (num_entries_nxt >= FIFO_depth) ? 1'b1 : 1'b0;
+	assign	empty_nxt 		= (num_entries_nxt <= 'd0       ) ? 1'b1 : 1'b0;
+	assign	data_avail  	= num_entries; //clk poedge num_entries <= num_entries_nxt 
+	assign  room_avail_nxt  = (FIFO_depth - num_entries_nxt); 
+		
+	always@(posedge clk or negedge reset)
+	begin
+		if(!reset)begin
+		wr_reg_ptr  <= 'd0				;
+		rd_reg_ptr  <= 'd0				;
+		full  	    <= 1'b0				;
+		empty 	    <= 1'b1				;
+		num_entries <= 'd0				; 
+		room_avail  <= FIFO_depth			;
+		end
+		else begin
+		wr_reg_ptr  <= wr_reg_ptr_nxt	;
+		rd_reg_ptr  <= rd_reg_ptr_nxt	;
+		full  	    <= full_nxt		;
+		empty 	    <= empty_nxt	;
+		num_entries <= num_entries_nxt  ;
+		room_avail  <= room_avail_nxt   ;
+		end
+	end
 
-    reg [FIFO_PTR:0]                num_entries     ;
-    reg [FIFO_PTR:0]                num_entries_nxt ;
+	// fifo writedata to memory or rddata from memory.	  
+	reg 	[FIFO_width-1:0] memory[FIFO_depth-1:0];
+	wire	[FIFO_width-1:0] memory_wire;
+	
+	assign  memory_wire = memory[0]; //memory[0] test pin
+	
+	always@(posedge clk or negedge reset)
+	begin
+		if(!reset) 
+			memory[wr_reg_ptr] <= 'h0000;
+		else if(wr_en == 1'b1)
+			memory[wr_reg_ptr] <= write_data ;				
+	end
 
-    wire                            full_nxt        ;
-    wire                            empty_nxt       ;
+	reg [FIFO_width-1:0] rddata;
+	
+	assign 	read_data = rddata;
+	
+	always@(posedge clk or negedge reset)
+	begin
+		if(!reset) 
+			rddata <=  'h0000 ;
+		else if (rd_en == 1'b1)
+			rddata <= memory[rd_reg_ptr] ;		
+	end
 
-    wire [FIFO_PTR:0]               room_avail_nxt  ;
-
-    //--------------------------------------------------------------------------
-    // write-pointer control logic
-    //--------------------------------------------------------------------------
-    always @(*)
-    begin 
-        wr_ptr_nxt = wr_ptr;
-        
-        if (write_en) begin
-            if (wr_ptr == FIFO_DEPTH_MINUS1)
-                wr_ptr_nxt = 'd0; //If address= 3'b111('d7)，下一個register寫入的adress為3'b000
-            else
-                wr_ptr_nxt = wr_ptr + 1'b1;
-        end
-    end 
-
-    //--------------------------------------------------------------------------
-    // read-pointer control logic
-    //--------------------------------------------------------------------------
-    always @(*)
-    begin 
-        rd_ptr_nxt = rd_ptr;
-        
-        if (read_en) begin
-            if (rd_ptr == FIFO_DEPTH_MINUS1)
-                rd_ptr_nxt = 'd0;
-            else
-                rd_ptr_nxt = rd_ptr + 1'b1;
-        end
-    end 
-
-    //--------------------------------------------------------------------------
-    // calculate number of occupied entries in the FIFO
-    //--------------------------------------------------------------------------
-    always @(*)
-    begin
-        num_entries_nxt = num_entries;
-
-        if (write_en && read_en)
-            num_entries_nxt = num_entries;
-        else if (write_en)
-            num_entries_nxt = num_entries + 1'b1;
-        else if (read_en)
-            num_entries_nxt = num_entries - 1'b1;
-    end
-
-    assign full_nxt         = (num_entries_nxt == FIFO_DEPTH);
-    assign empty_nxt        = (num_entries_nxt == 'd0);
-    assign data_avail       = num_entries;
-    assign room_avail_nxt   = (FIFO_DEPTH - num_entries_nxt);
-
-    //--------------------------------------------------------------------------
-    // register output
-    //--------------------------------------------------------------------------
-    always @(posedge clk or negedge rst_n)
-    begin
-        if (!rst_n) begin
-            wr_ptr      <= 'd0;
-            rd_ptr      <= 'd0;
-            num_entries <= 'd0;
-            full        <= 1'b0;
-            empty       <= 1'b1;
-            room_avail  <= FIFO_DEPTH;
-        end
-        else begin
-            wr_ptr      <= wr_ptr_nxt;
-            rd_ptr      <= rd_ptr_nxt;
-            num_entries <= num_entries_nxt;
-            full        <= full_nxt;
-            empty       <= empty_nxt;
-            room_avail  <= room_avail_nxt;
-        end
-    end 
-
-    //--------------------------------------------------------------------------
-    // SRAM memory instantiation
-    //--------------------------------------------------------------------------
-    sram
-    #(
-	.PTR			(FIFO_PTR		),
-	.FIFO_WIDTH		(FIFO_WIDTH		)
-    )
-    u_sram
-    (
-	.wrclk			(clk			),
-	.wren			(write_en		),
-	.wrptr			(wr_ptr			),
-	.wrdata			(write_data		),
-	.rdclk			(clk			),
-	.rden			(read_en		),
-	.rdptr			(rd_ptr			),
-	.rddata			(read_data		)
-    );
-
+	
 endmodule
 
-`timescale 1ns / 1ps
-
-module sram
-#(
-    //----------------------------------
-    // Paramter Declarations
-    //----------------------------------
-    parameter PTR 			= 3			,
-    parameter FIFO_WIDTH		= 16			,
-    parameter A_MAX 			= 2**(PTR)
-)
-(
-    //----------------------------------
-    // IO Declarations
-    //----------------------------------
-    // Write port
-    input                		wrclk			,
-    input [PTR-1:0] 			wrptr			,
-    input [FIFO_WIDTH-1:0] 		wrdata			,
-    input                		wren			,
-
-    // Read port
-    input           			rdclk   		,
-    input [PTR-1:0] 			rdptr			,
-    input                		rden			,
-    output reg [FIFO_WIDTH-1:0]         rddata			
-);
-
-    //----------------------------------
-    // Variable Declarations
-    //----------------------------------
-    // Memory as multi-dimensional array
-    reg [FIFO_WIDTH-1:0] 		memory[A_MAX-1:0]; // ==8個[15:0]memory
-	
-    //----------------------------------
-    // Start of Main Code
-    //----------------------------------
-	
-    // Write data to memory
-    always @(posedge wrclk) 
-    begin
-        if (wren) begin
-	    memory[wrptr] <= wrdata;	//
-	end
-    end
-
-    // Read data from memory
-    always @(posedge rdclk) 
-    begin
-	if (rden) begin
-	    rddata <= memory[rdptr];
-	end
-    end
-	
-endmodule
