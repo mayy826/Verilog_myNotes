4617b6a2bbb0ae1186a4573dfd52553ff433bdf1
mayy826 <52116916+mayy826@users.noreply.github.com>
2023-12-21 11:35:36 +0800

    Update tb_synch_fifo.v

--- fifo/tb_synch_fifo.v
+++ fifo/tb_synch_fifo.v
@@ -1,44 +1,44 @@
-`timescale  1ns / 100ps      
+`timescale  1ns / 1ps      
 
 module fifo_anya_tb();
 
-// fifo Inputs
-reg         clk		= 1'b0 ;
-reg         reset	= 1'b0 ;
-reg         rd_en 	= 1'b0 ;
-reg         wr_en 	= 1'b0 ;
-reg  [15:0] write_data	   ;
-
-
-// fifo Outputs
-wire [15:0] read_data	   ;
-wire 		full, empty    ;
-wire		full_nxt, empty_nxt;
-wire [3:0]  room_avail, data_avail;
-wire [15:0]  memory_wire;
-
-// clk  
-always  
-    begin
-      #10 clk = ~clk;
-    end	
-
-synch_fifo test_instance(
-		.clk		(clk)  		, 
-		.reset		(reset)		, 
-		.rd_en		(rd_en)		, 
-		.wr_en		(wr_en)		, 
-		.write_data (write_data), 
-		.read_data  (read_data) , 
-		.full		(full) , 
-		.empty		(empty),
-		.full_nxt   (full_nxt) ,
-		.empty_nxt	(empty_nxt) ,	
-		.room_avail	(room_avail), 
-		.data_avail (data_avail),
-		.memory_wire( memory_wire)
-);
-
+	// fifo Inputs
+	reg         clk		= 1'b0 ;
+	reg         reset	= 1'b0 ;
+	reg         rd_en0	= 1'b0 ;
+	reg         wr_en0 	= 1'b0 ;
+	reg  [15:0] write_data	   ;
+	
+	
+	// fifo Outputs
+	wire [15:0] read_data	   		  ;
+	wire 		full, empty    		  ;
+	wire		full_nxt, empty_nxt	  ;
+	wire [3:0]  room_avail, data_avail;
+	wire [15:0]  memory_wire		  ;
+	
+	// clk  
+	always  
+		begin
+		#10 clk = ~clk;
+		end	
+	
+	synch_fifo test_instance(
+			.clk		(clk)  			, 
+			.reset		(reset)			, 
+			.rd_en0		(rd_en0)		, 
+			.wr_en0		(wr_en0)		, 
+			.write_data (write_data)	, 
+			.read_data  (read_data) 	, 
+			.full		(full) 			, 
+			.empty		(empty)			,
+			.full_nxt   (full_nxt) 		,
+			.empty_nxt	(empty_nxt) 	,	
+			.room_avail	(room_avail)	, 
+			.data_avail (data_avail)	,
+			.memory_wire( memory_wire)
+	);
+	
 
     initial
     begin
@@ -65,46 +65,46 @@ synch_fifo test_instance(
 //	write fifo task
 //-----------------------------------
     task write_fifo;
-		input        full ;
-        input [15:0]  value;
+		input        full 		;
+        input [15:0]  value		;
 	begin
-     	if(full == 1'b1)
-		wr_en 	  <= 1'b0;
-		else begin		
+     	/*if(full == 1'b1)
+		wr_en0 	  <= 1'b0;
+		else begin*/		
 		@(posedge clk);
-        wr_en     <= 1'b1;
-        write_data  <= value   ;
+        wr_en0     <= 1'b1		;
+        write_data  <= value    ;
         @(posedge clk);
-        wr_en     <= 1'b0;	
+        wr_en0     <= 1'b0;	
 		@(posedge clk);	
-		end           
+		//end           
     end
 	endtask
 
 //  read fifo task
 //-----------------------------------
 	task read_fifo;
-		input        empty /
+		input        empty; 
 	begin
-		if(empty == 1'b1)
-		rd_en 	  <= 1'b0;
-		else begin		
+		/*if(empty == 1'b1)
+		rd_en0 	  <= 1'b0;
+		else begin*/		
 		@(posedge clk);
-        rd_en     <= 1'b1;
+        rd_en0     <= 1'b1;
         @(posedge clk);
-        rd_en     <= 1'b0;	
-		@(posedge clk);	
-		end
-	end
+        rd_en0     <= 1'b0;	
+		@(posedge clk);
+        //end
+    end
 	endtask
 
 
 // read after write task
 //------------------------------------
     task read_after_write;
-	input [31:0] 	num_write   ; //write 幾次
-	reg [7:0] 		idx			; //第幾次trigger
-      reg [15:0] 	valW			;
+		input [31:0] 	num_write   ; //write 幾次
+		reg [7:0] 		idx			; //第幾次trigger
+		reg [15:0] 	valW			;
 
 
 	for (idx = 0; idx < 8 ; idx = idx + 1) begin
@@ -123,12 +123,12 @@ synch_fifo test_instance(
         reg [15:0]   valC        ;
 
     begin
-        for (index = 0; index < 9; index = index + 1) begin //讓fifo滿
+        for (index = 0; index < 9; index = index + 1) begin
             valW = ~(index + 1);
             write_fifo(full,valW);
         end
 
-        for (index = 0; index < 20; index = index + 1) begin //空了繼續讀fifo
+      for (index = 0; index < 10; index = index + 1) begin
             valC = ~(index + 1);
             read_fifo(empty);          
         end
